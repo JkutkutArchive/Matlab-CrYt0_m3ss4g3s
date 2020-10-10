a76975904684712c7b82e0f0d3d253e8464a4344
jkutkut <jorgeregonzalez@gmail.com>
2020-10-11 01:06:36 +0200

    working on charToMatrix

--- generate.m
+++ generate.m
@@ -1,12 +1,28 @@
 % message = "hello world";
 message = "hello";
-for i=1:size
-   for j = 1:size
-      for k = 1:size
-          r = rand();
-          if r < 0.2
-              m(i,j,k) = i + 2 * j + 4 * k - 6;
-          end
-      end
-  end
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Matrix natural order
+% for i=1:size
+%    for j = 1:size
+%       for k = 1:size
+%           r = rand();
+%           if r < 0.2
+%               m(i,j,k) = i + 2 * j + 4 * k - 6;
+%           end
+%       end
+%   end
+% end
