b6ee73798d173143587707ac7ffe1179ade6add6
jkutkut <jorgeregonzalez@gmail.com>
2020-10-11 00:09:29 +0200

    files added to manage code

--- visualization.m
+++ visualization.m
@@ -1,11 +1,3 @@
-% Constants:
-size = 5;
-m = zeros(size, size, size);
-vec = 1:size;
-
-% Visuals:
-offset = 0.1;
-
 plot3(...
     vec, vec, vec * 0, "b",...
     flip(vec), vec, vec * 0, "b",...
@@ -13,18 +5,6 @@ plot3(...
     flip(vec), vec + flip(vec), vec, "b",...
     vec + flip(vec), vec, vec, "b",...
     vec + flip(vec), flip(vec), vec, "b")
-
-for i=1:size
-   for j = 1:size
-      for k = 1:size
-          r = rand();
-          if r < 0.2
-              m(i,j,k) = i + 2 * j + 4 * k - 6;
-          end
-      end
-  end
-end
-
 
 for i=1:size
    for j = 1:size
