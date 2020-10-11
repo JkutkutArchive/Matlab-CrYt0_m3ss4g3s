7cfe5f2373dca8625d33fcae58abfaf0d4692325
jkutkut <jorgeregonzalez@gmail.com>
2020-10-11 02:40:10 +0200

    Stable

--- showM.m
+++ showM.m
@@ -1,7 +1,8 @@
 function [] = showM(m)
-    if ndims(m) <= 2
-        for i = 1:length(m(:,1))
-            for j = 1:length(m(1,:))
+    if ndims(m) == 2 % 2D MATRIX
+        [A, B] = size(m);
+        for i = 1:A
+            for j = 1:B
                 if m(i,j)
                     fprintf("0")
                 else
@@ -10,7 +11,8 @@ function [] = showM(m)
             end
             fprintf("\n")
         end
-    elseif ndims(m) == 3    
+        
+    elseif ndims(m) == 3 % 3D MATRIX
         plot3(...
             vec, vec, vec * 0, "b",...
             flip(vec), vec, vec * 0, "b",...
@@ -22,11 +24,12 @@ function [] = showM(m)
         grid
         axis([-offset, size + 1 + offset, -offset, size + 1 + offset, -offset, size + 1 + offset])
 
+        [A, B, C] = size(m)
         switch visuType
             case "normal"
-                for i=1:length(m(:, 1, 1))
-                    for j = 1:length(m(1, :, 1))
-                        for k = 1:length(m(1, 1, :))
+                for i= 1:A
+                    for j = 1:B
+                        for k = 1:C
                             if ~m(i,j,k) == 0
                                 text(i, j, k, string(m(i,j,k)))
                             end
@@ -38,15 +41,17 @@ function [] = showM(m)
                 s = visuType;
         end
 
-        for i=1:size
-           for j = 1:size
-              for k = 1:size
+        for i=1:A
+           for j = 1:B
+              for k = 1:C
                   if ~m(i,j,k) == 0
                       text(i, j, k, s)
                   end
               end
           end
         end
+    else % if other dimesion, just print it
+        m
     end
 end
 
